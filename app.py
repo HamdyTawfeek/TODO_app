4fea01bfca320b0928ca8819894cbae02446887a
Hamdy <hamdy.t@trufla.com>
2020-04-18 15:24:01 +0200

    basic structure of the app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = [{'description': 'Todo 1 '},
+            {'description': 'Todo 2 '},
+             {'description': 'Todo 3 '}
+            ]
+    return render_template('index.html', data=data)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
