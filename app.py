bc354428e842e86d57ddfc1f1cc3a42ac28b0f67
Hamdy <hamdy.t@trufla.com>
2020-04-18 15:50:28 +0200

    connecting to postgres and replacing static data with database records

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+username, password, port= 'postgres', '7490', 5432
+DB_URI = f"postgresql://{username}:{password}@localhost:{port}/todoapp"
+
+
+###In psql: sudo su - postgres
+###In psql: createdb -h localhost -p 5432 -U postgres todoapp
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    data = [{'description': 'Todo 1 '},
-            {'description': 'Todo 2 '},
-             {'description': 'Todo 3 '}
-            ]
+    data = Todo.query.all()
     return render_template('index.html', data=data)
-
-
 
 
 if __name__ == '__main__':
