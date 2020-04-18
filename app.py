ba9849dfad6ae1b7cd0f3a025c3035a164684c28
Hamdy <hamdy.t@trufla.com>
2020-04-18 16:48:51 +0200

    feat: allowing user to add item

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -30,6 +30,14 @@ def index():
     data = Todo.query.all()
     return render_template('index.html', data=data)
 
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo_item = Todo(description=description)
+    db.session.add(todo_item)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
