42b0c2d61e1f416dfc633c3241cfd55d1cea5707
Hamdy <hamdy.t@trufla.com>
2020-04-19 10:55:46 +0200

    added a completed column to the database and included migrations as versions of the database

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-
 username, password, port= 'postgres', '7490', 5432
 DB_URI = f"postgresql://{username}:{password}@localhost:{port}/todoapp"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
 
 
 ###In psql: sudo su - postgres
 ###In psql: createdb -h localhost -p 5432 -U postgres todoapp
 
-app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+#db.create_all()
 
 @app.route('/')
 def index():
